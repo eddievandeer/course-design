7e7539d3f8868614ed2d1c5ae27872b41028a1c3
eddievandeer <1255173159@qq.com>
2020-12-21 18:28:25 +0800

    refactor: modifiy App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+    <router-view></router-view>
 </template>
 
 <script>
-  export default {
-    name: 'App'
-  }
+    export default {
+        name: 'App'
+    }
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+    body {
+        margin: 0;
+        padding: 0;
+    }
 </style>
