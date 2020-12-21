4760eb5e82da85d786ebe19639526c5d7a2cafbd
eddievandeer <1255173159@qq.com>
2020-12-21 10:44:22 +0800

    feat: add vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routers'
+import store from './store'
 import 'font-awesome/css/font-awesome.min.css'
 
 createApp(App)
+    .use(store)
     .use(router)
     .mount('#app')
