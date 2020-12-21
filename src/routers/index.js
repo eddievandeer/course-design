1ebe961f598e9f05c20a51fac7043f2586e4f343
eddievandeer <1255173159@qq.com>
2020-12-21 18:30:16 +0800

    feat: add routers

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
+    {
+        path: '/',
+        redirect: '/home'
+    },
+    {
+        path: '/home',
+        name: 'Home',
+        component: () => import('../views/Home.vue')
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: () => import('../views/Login.vue')
+    }
 ]
 
 const router = createRouter({
