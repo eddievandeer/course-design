61c4defa6537da6e012fd7853b84616cafad4e6d
eddievandeer <1255173159@qq.com>
2020-12-23 09:01:10 +0800

    feat: achieve room page

---
+++ src/components/room/RoomCard.vue
@@ -0,0 +1,139 @@
+<template>
+    <!-- 房间卡片 -->
+    <div class="room-item">
+        <div class="room-pic">
+            <img :src="room.image" alt="">
+        </div>
+        <div class="room-info">
+            <p class="room-type">{{room.type}}</p>
+            <p class="room-description">{{room.description}}</p>
+            <p class="room-surplus">剩余房间数：{{room.surplus}}</p>
+        </div>
+        <div class="room-reserve">
+            <div class="room-price">
+                <span>￥</span><span class="price">{{room.price}}</span>起
+            </div>
+            <router-link :to="{ name: 'Reserve', params: { type: room.type } }" v-if="room.surplus > 0" class="reserve">
+                <button>预订</button>
+            </router-link>
+            <button v-else class="reserve-disable">无空闲</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'RoomCard',
+        props: ['room']
+    }
+</script>
+
+<style lang="scss" scoped>
+    .room-item {
+        width: 60%;
+        padding: .8rem;
+        background-color: white;
+        margin-top: 0.5rem;
+        box-sizing: border-box;
+        position: relative;
+        display: flex;
+
+        .room-pic {
+            width: auto;
+            height: 8rem;
+
+            img {
+                height: 100%;
+                object-fit: cover;
+                border-radius: 5px;
+            }
+        }
+
+        p {
+            margin: 0;
+            color: #3f3f3f;
+        }
+
+        .room-info {
+            margin-left: 1.8rem;
+
+            .room-type {
+                font-size: 20px;
+                font-weight: bold;
+            }
+
+            .room-description {
+                font-size: 16px;
+                margin-top: .2rem;
+            }
+
+            .room-surplus {
+                font-size: 14px;
+                margin-top: .1rem;
+            }
+        }
+
+        .room-reserve {
+            position: absolute;
+            right: 1rem;
+            bottom: 1rem;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+
+            .room-price {
+                color: #515151;
+                font-size: 14px;
+
+                span {
+                    font-size: 16px;
+                    color: #F73B0F;
+                }
+
+                .price {
+                    font-size: 28px;
+                    margin-right: 0.1rem;
+                }
+            }
+
+            .reserve-disable,
+            .reserve button {
+                width: 5rem;
+                height: 2rem;
+                color: white;
+                border-radius: 4px;
+                border: none;
+                outline: none;
+                background-color: #F73B0F;
+                cursor: pointer;
+            }
+
+            .reserve-disable {
+                background-color: #db806b;
+                cursor: not-allowed;
+            }
+        }
+    }
+
+    @media screen and (max-width: 768px) {
+        .room-item {
+            width: 100%;
+            padding: .6rem;
+
+            .room-pic {
+                height: auto;
+
+                img {
+                    width: 6rem;
+                    height: 6rem;
+                    object-fit: cover;
+                }
+            }
+
+            .room-info {
+                margin-left: 1rem;
+            }
+        }
+    }
+</style>
---
+++ src/components/room/RoomList.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="room-list-wrapper">
+        <room-card v-for="(item,index) in rooms" :key="index" :room="item"></room-card>
+    </div>
+</template>
+
+<script>
+    import {
+        reactive
+    } from 'vue'
+    import RoomCard from './RoomCard'
+
+    export default {
+        components: {
+            RoomCard
+        },
+        name: 'RoomList',
+        setup() {
+            let rooms = reactive([{
+                    type: '高级间',
+                    description: '大床  26㎡  有窗',
+                    image: 'https://i.loli.net/2020/12/21/Vx9HfIBCRbTDvKc.jpg',
+                    price: 658,
+                    surplus: 10
+                },
+                {
+                    type: '高级间',
+                    description: '大床  26㎡  有窗',
+                    image: 'https://i.loli.net/2020/12/21/Vx9HfIBCRbTDvKc.jpg',
+                    price: 658,
+                    surplus: 0
+                },
+                {
+                    type: '高级间',
+                    description: '大床  26㎡  有窗',
+                    image: 'https://i.loli.net/2020/12/21/Vx9HfIBCRbTDvKc.jpg',
+                    price: 658,
+                    surplus: 10
+                },
+                {
+                    type: '高级双床房',
+                    description: '双床  30㎡  有窗',
+                    image: 'https://i.loli.net/2020/12/21/Vx9HfIBCRbTDvKc.jpg',
+                    price: 658,
+                    surplus: 10
+                },
+                {
+                    type: '高级间',
+                    description: '大床  26㎡  有窗',
+                    image: 'https://i.loli.net/2020/12/21/Vx9HfIBCRbTDvKc.jpg',
+                    price: 658,
+                    surplus: 10
+                }
+            ])
+
+            return {
+                rooms
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .room-list-wrapper {
+        width: 100%;
+        background-color: #F3F3F4;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
