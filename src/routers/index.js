2acfb89ae92cb457f6a5c011a5aa612c24d55fd1
eddievandeer <1255173159@qq.com>
2020-12-24 21:13:57 +0800

    feat(routers): add login router and reserve router

--- src/routers/index.js
+++ src/routers/index.js
@@ -6,20 +6,53 @@ const routes = [
         redirect: '/home'
     },
     {
+        path: '/login',
+        name: 'Login',
+        component: () => import('../views/Login.vue'),
+        children: [
+            {
+                path: '/login',
+                name: 'LoginBox',
+                component: () => import('../components/user/LoginBox.vue')
+            },
+            {
+                path: '/register',
+                name: 'RegisterBox',
+                component: () => import('../components/user/RegisterBox.vue')
+            }
+        ]
+    },
+    {
         path: '/home',
         name: 'Home',
         component: () => import('../views/Home.vue')
     },
     {
-        path: '/login',
-        name: 'Login',
-        component: () => import('../views/Login.vue')
+        path: '/reserve/:type',
+        name: 'Reserve',
+        component: () => import('../views/Reserve.vue'),
+        props: true,
+        children: [
+            {
+                path: '/reserve/:type/:rid',
+                name: 'RoomDetail',
+                component: () => import('../components/reserve/RoomDetail.vue'),
+                props: true
+            }
+        ]
     }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.name !== 'Home') {
+        next()
+    }
+    else next()
 })
 
 export default router
