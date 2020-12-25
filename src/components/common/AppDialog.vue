ceeec0a0914d379d06639e59a8716a40668750de
eddievandeer <1255173159@qq.com>
2020-12-25 10:01:35 +0800

    feat(components): add common components

---
+++ src/components/common/AppDialog.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="dialog-wrapper" v-if="show">
+        <div class="dialog" :class="type">
+            <div class="dialog-icon" :class="type">
+                <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+            </div>
+            <span class="content">
+                <slot></slot>
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppDialog',
+        props: {
+            show: Boolean,
+            type: String
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .dialog-wrapper {
+        z-index: 10;
+        position: fixed;
+        top: 1.2rem;
+        left: 50%;
+        transform: translateX(-50%);
+
+        .dialog {
+            height: 3rem;
+            border-radius: 4px;
+            padding: 0 1rem;
+            background-color: white;
+            display: flex;
+            align-items: center;
+
+            &.error {
+                border: 1px solid #fde2e2;
+                background-color: #FEF0F0;
+            }
+
+            .dialog-icon {
+                font-size: 14px;
+                margin-right: 0.6rem;
+
+                &.error {
+                    color: #D95C5C;
+                }
+            }
+        }
+    }
+</style>
---
+++ src/components/common/Loading.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="loading-wrapper" v-if="show">
+        <div class="mask"></div>
+        <div class="loading">
+            <i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+            <span class="content">
+                <slot></slot>
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Loading',
+        props: {
+            show: Boolean
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .loading-wrapper {
+        z-index: 10;
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: fixed;
+        top: 0;
+        left: 0;
+
+        .mask {
+            z-index: 10;
+            width: 100%;
+            height: 100%;
+            position: fixed;
+            top: 0;
+            left: 0;
+        }
+
+        .loading {
+            z-index: 20;
+            width: 12rem;
+            height: 12rem;
+            color: white;
+            border-radius: 10px;
+            background-color: rgba($color: #000000, $alpha: .5);
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+
+            .content {
+                font-size: 18px;
+                margin-top: 1rem;
+            }
+        }
+    }
+</style>
