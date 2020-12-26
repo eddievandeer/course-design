4674948da14d7d7825ebd79759d089b7578cee60
eddievandeer <1255173159@qq.com>
2020-12-26 09:25:45 +0800

    feat: import global style file

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './routers'
 import store from './store'
 import 'font-awesome/css/font-awesome.min.css'
+import '../src/style/global.scss'
 
 createApp(App)
     .use(store)
