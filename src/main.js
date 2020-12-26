be0008300b79c4e9e3e48a82449037441ed4e9b7
eddievandeer <1255173159@qq.com>
2020-12-26 21:40:00 +0800

    feat: register global components

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import router from './routers'
 import store from './store'
 import 'font-awesome/css/font-awesome.min.css'
 import '../src/style/global.scss'
+import AppDialog from './components/common/AppDialog.vue'
+import Loading from './components/common/Loading.vue'
 
-createApp(App)
-    .use(store)
-    .use(router)
-    .mount('#app')
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.mount('#app')
+app.component('app-dialog', AppDialog)
+app.component('loading', Loading)
