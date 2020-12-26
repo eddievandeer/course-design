00905c98a4fd3350f7680d0eecbac2f464e44757
eddievandeer <1255173159@qq.com>
2020-12-26 09:37:55 +0800

    fix(store): handle some exceptions

--- src/store/index.js
+++ src/store/index.js
@@ -8,16 +8,22 @@ export default createStore({
     mutations: {
         SET_USER_DATA(state, data) {
             state.user = data
+            localStorage.setItem('user', JSON.stringify(data))
         },
         CLEAR_USER_DATA(state) {
             state.user = null
+            localStorage.removeItem('user')
         }
     },
     actions: {
-        async doLogin({ commit }, param) {
-            const response = await login(param)
-            console.log(response)
-            commit('SET_USER_DATA', response.data)
+        doLogin({ commit }, param) {
+            const response = login(param)
+            response.then((result) => {
+                if (result.data.code == 200) {
+                    commit('SET_USER_DATA', result.data)
+                }
+            })
+            return response
         },
         doLogout({ commit }) {
             commit('CLEAR_USER_DATA')
