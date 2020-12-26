c81ff90c276942ea8af276d8d0bf0be45a060eb0
eddievandeer <1255173159@qq.com>
2020-12-26 09:41:12 +0800

    feat(services): add room data acquisition request

--- src/services/index.js
+++ src/services/index.js
@@ -1,14 +1,23 @@
 import http from '../config/http'
 
-function login(params) {
+export function login(params) {
     return http.post('/user/login', params)
 }
 
-function register(params) {
+export function register(params) {
     return http.post('/user/register', params)
 }
 
-export {
-    login,
-    register
-}
+export const getAllRooms = () => http.get('/room/types')
+
+export const getTypeData = (type) => http.get('/data/getTypeData', {
+    params: {
+        tid: type
+    }
+})
+
+export const getData = (rid) => http.get('/data/get', {
+    params: {
+        rid
+    }
+})
